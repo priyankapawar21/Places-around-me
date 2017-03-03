86bd73676a2dbf059b76e58b817f8c5db06f1af3
Mac <mac@Macs-iMac.local>
2017-03-03 20:04:58 +0530

    Initial Commit

---
+++ new-place-find/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  new-place-find
+//
+//  Created by Mac on 03/03/17.
+//  Copyright © 2017 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
